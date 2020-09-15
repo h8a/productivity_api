f4fc10893ff7b205183de491a1cf3f7f32036f43
hery8a <herii8a@gmail.com>
2020-09-15 17:37:48 -0600

    [ADD] se agrega status_task

--- src/db.py
+++ src/db.py
@@ -1,4 +1,5 @@
 import random
+from datetime import datetime, timedelta
 from faker import Faker
 
 
@@ -35,21 +36,35 @@ async def generate_random_data(app):
 
     fake = Faker()
 
+    now = datetime.now()
+
+    status_task = 'stop'
+
     for i in range(1, 51):
         optionalMinutes = random.randint(15, 120) * 60
+
+        date = (now - timedelta(hours=i)).strftime('%Y-%m-%d %H:%M:%S')
 
         if i % 2:
             time_complete = int((80 * optionalMinutes) / 100.0)
         else:
             time_complete = int((100 * optionalMinutes) / 100.0)
 
+        if (timedelta == 0){
+            status_task = 'complete'
+        } else {
+            status_task = 'stop'
+        }
+
         app['db']['tasks'].append({
             'id': i,
+            'create_date': date,
+            'update_date': date,
             'description': fake.name(),
             'status': '1',
             'optionalMinutes': optionalMinutes,
             'optionalSeconds': 0,
             'status_time': time_complete,
-            'status_task': 'stop',
+            'status_task': status_task,
             'duration': 0,
         })
